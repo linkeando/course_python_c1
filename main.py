8145333903198042d027907a54a3e7197a96fb3e
xtream-club <nanguelulpz@gmail.com>
2023-11-27 14:21:33 -0600

    initial data

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ app = FlaskApp().create_app()
 route_manager = RouteManager(app, "application.backend.routes").register_routes()
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
-    #serve(app, host='0.0.0.0', port=3000)
+    serve(app, host='0.0.0.0', port=3000)
